da1816f1cf44ced0e216b78434bf86441eded98e
Citysquirrel <tok1324@naver.com>
2024-05-13 04:05:54 +0900

    유튜브 비디오 컴포넌트

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -1,6 +1,6 @@
 import { useRecoilState } from "recoil";
 import { PlatformInfosDetail, stellarState } from "../lib/Atom";
-import { Card, CardBody, CardHeader, HStack, SimpleGrid, Skeleton, Stack, Text, VStack, theme } from "@chakra-ui/react";
+import { Card, CardBody, CardHeader, HStack, SimpleGrid, Stack, Text, theme } from "@chakra-ui/react";
 import { Spacing } from "../components/Spacing";
 import { Image } from "../components/Image";
 import symbolTabi from "../assets/symbol/symbol_tabi.png";
--- src/pages/YoutubeVideo.tsx
+++ src/pages/YoutubeVideo.tsx
@@ -1,3 +1,19 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { stellarState } from "../lib/Atom";
+import { NAME_TABI } from "../lib/constant";
+
 export function YoutubeVideo() {
+	const [stellar, setStellar] = useRecoilState(stellarState);
+	// data[0].youtube?.videos[0].?
+	const tabi = stellar.filter((s) => s.name === NAME_TABI);
+	return (
+		<div>
+			<SimpleGrid as="section" columns={[2, 3, 4]} spacing="12px"></SimpleGrid>
+		</div>
+	);
+}
+
+export function VideoCard() {
 	return <></>;
 }
