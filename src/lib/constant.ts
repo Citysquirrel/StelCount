32477bfee3dc90cf2bc7510c2e7421832a1c3ede
Citysquirrel <tok1324@naver.com>
2024-05-13 04:05:30 +0900

    고유명사

---
+++ src/lib/constant.ts
@@ -0,0 +1,8 @@
+const NAME_KANNA = "아이리 칸나";
+const NAME_YUNI = "아야츠노 유니";
+const NAME_TABI = "아라하시 타비";
+const NAME_MASHIRO = "네네코 마시로";
+const NAME_HINA = "시라유키 히나";
+const NAME_LIZE = "아카네 리제";
+
+export { NAME_KANNA, NAME_YUNI, NAME_TABI, NAME_MASHIRO, NAME_HINA, NAME_LIZE };
