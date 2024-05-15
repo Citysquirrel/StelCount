c1bdfad036814eb489ff81427b23b4af10f63951
Citysquirrel <tok1324@naver.com>
2024-05-15 18:40:56 +0900

    세팅 변경 및 번들옵션 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dependencies } from "./package.json";
+
+const vendor = ["react", "react-router-dom", "react-dom"];
+const libs = ["uuid", "axios", "immutability-helper", "file-saver", "react-dnd", "react-dnd-html5-backend"];
+const excludes = [...vendor, ...libs, "aws-sdk"];
+
+function renderChunks(deps: Record<string, string>) {
+	let chunks: { [key: string]: string[] } = {};
+	Object.keys(deps).forEach((key) => {
+		if (excludes.includes(key)) return;
+		// const uuidKey = uuidv4();
+		chunks[key] = [key];
+	});
+	return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor,
+					libs,
+					...renderChunks(dependencies),
+				},
+			},
+		},
+	},
 });
 
