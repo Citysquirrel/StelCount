02f2e725b23b4d6a6fcbbaff667a467241d286c2
Citysquirrel <tok1324@naver.com>
2024-05-15 19:51:10 +0900

    env.d.ts 정리

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-	readonly VITE_SERVER_URL: string;
-	readonly VITE_YOUTUBE_API_KEY: string;
-	readonly VITE_NAVER_CLIENT_ID: string;
-	readonly VITE_REDIRECT_URL: string;
-}
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+	readonly VITE_SERVER_URL: string;
+	readonly VITE_YOUTUBE_API_KEY: string;
+	readonly VITE_NAVER_CLIENT_ID: string;
+	readonly VITE_REDIRECT_URL: string;
+}
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
+
