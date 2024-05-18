02ca33a70d37112bef2df0a44fd731dcd8701949
Citysquirrel <tok1324@naver.com>
2024-05-19 04:40:57 +0900

    delete unused phrases

--- src/lib/Provider.tsx
+++ src/lib/Provider.tsx
@@ -1,9 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
 import { RecoilRoot } from "recoil";
 import theme from "./theme";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { fetch_ } from "./functions/fetch";
 
 //! fetch를 활용하면서 어떤식으로 작동하는지, 어떤것들이 개선되면 좋은지를 체험
 // const queryClient = new QueryClient({
