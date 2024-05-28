67e6fa127350210b6a76221695eda3709b459b87
Citysquirrel <tok1324@naver.com>
2024-05-28 18:01:45 +0900

    dev mod in useConsole

--- src/lib/hooks/useConsole.ts
+++ src/lib/hooks/useConsole.ts
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 
 export function useConsole(value: any, name: string | undefined = undefined) {
 	useEffect(() => {
-		if (!name) {
-			console.log(value);
-		} else console.log({ [name]: value });
+		if (import.meta.env.DEV)
+			if (!name) {
+				console.log(value);
+			} else console.log({ [name]: value });
 	}, [value]);
 	return value;
 }
