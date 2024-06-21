0c97ec899688e8f8f295057b78c84b21f92cc70f
Citysquirrel <tok1324@naver.com>
2024-06-21 17:38:27 +0900

    home page not ready

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,45 @@
-import { HStack, Heading, Stack } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Card, CardBody, HStack, Heading, Stack } from "@chakra-ui/react";
+import { useEffect, useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useBackgroundColor from "../lib/hooks/useBackgroundColor";
 
 // TODO: 여기서는 현재 활성중이거나 곧 다가오는 기념일 목록을 보여줍니다.
 // Card or List 형태?
 export default function Home() {
+	const nav = useNavigate();
 	useBackgroundColor("blue.50");
+	useLayoutEffect(() => {
+		import.meta.env.PROD && nav("/counter");
+	}, []);
 	return (
-		<Stack minHeight="calc(100vh - 124px)" gap={"8px"}>
-			{/* 최상단에 최근 이벤트 크게 렌더 */}
-			<CarouselList heading={"주목할 음악 영상"} contents={[]} />
-			<CarouselList heading={"주목할 음악 영상"} contents={[]} />
-			<CarouselList heading={"치지직 라이브 현황"} contents={[]} />
+		<Stack minHeight="calc(100vh - 124px)" alignItems={"center"}>
+			<Stack
+				width={["100%", "100%", "100%", "768px", "1024px"]}
+				padding={["12px", "12px", "12px", null, null]}
+				gap={"8px"}
+			>
+				{/* 최상단에 최근 이벤트 크게 렌더 */}
+				<RecentNews />
+				<CarouselList heading={"주목할 음악 영상"} contents={[]} />
+				<CarouselList heading={"주목할 음악 영상"} contents={[]} />
+				<CarouselList heading={"치지직 라이브 현황"} contents={[]} />
+			</Stack>
 		</Stack>
+	);
+}
+
+function RecentNews() {
+	return (
+		<Card>
+			<CardBody></CardBody>
+		</Card>
 	);
 }
 
 function CarouselList({ heading, contents }: CarouselListProps) {
 	return (
 		<Stack>
-			<Heading>{heading}</Heading>
+			<Heading size="sm">{heading}</Heading>
 			<HStack>
 				{contents.map((c) => (
 					<></>
