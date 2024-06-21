3b01ab86a111c2de63ad241a138157b5b265386d
Citysquirrel <tok1324@naver.com>
2024-06-21 18:40:21 +0900

    change small loading circle to svg tags

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -39,14 +39,31 @@ export function LoadingAtCorner() {
 				alignItems: "center",
 				justifyContent: "center",
 				zIndex: 1001,
-				"> img": {
+				"> svg": {
 					boxSize: "32px",
 					animation: "rotate 1s ease infinite",
 					transformOrigin: "50% 50%",
 				},
 			}}
 		>
-			<Image src={"/images/load_donut.png"} />
+			<svg
+				width="100"
+				height="100"
+				viewBox="0 0 100 100"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				stroke="#3498db"
+				strokeWidth="10"
+			>
+				<circle cx="50" cy="50" r="40" strokeOpacity="0.3" />
+				<circle
+					cx="50"
+					cy="50"
+					r="40"
+					strokeDasharray="62.83185307179586 62.83185307179586"
+					strokeDashoffset="62.83185307179586"
+				/>
+			</svg>
 		</Stack>
 	);
 }
