7d9815df9dd12c8f49cfb1e060fee8b0d3a29e20
Citysquirrel <tok1324@naver.com>
2024-06-25 18:21:29 +0900

    edit rate limited msg

--- src/pages/ServerErrorPage.tsx
+++ src/pages/ServerErrorPage.tsx
@@ -7,7 +7,7 @@ export function ServerErrorPage() {
 	const [serverError] = useRecoilState(serverErrorState);
 	const { pathname } = useLocation();
 	const errorMsg = {
-		429: { title: "Too Many Requests", description: "분당 요청 횟수를 초과했습니다. 잠시 후 다시 시도해주세요." },
+		429: { title: "Too Many Requests", description: "요청 횟수를 초과했습니다. 잠시 후 다시 시도해주세요." },
 		500: {
 			title: "Internal Server Error",
 			description: "내부 서버 에러입니다. 지속 발생 시 개발자에게 문의 바랍니다.",
