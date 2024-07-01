b6af0f51a621c2f6f98b4b23fef47bca98363f0e
Citysquirrel <tok1324@naver.com>
2024-07-01 14:08:42 +0900

    create useConsoleAdmin

--- src/lib/hooks/useConsole.ts
+++ src/lib/hooks/useConsole.ts
@@ -1,8 +1,21 @@
 import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { isAdminState } from "../Atom";
 
 export function useConsole(value: any, name: string | undefined = undefined) {
 	useEffect(() => {
 		if (import.meta.env.DEV)
+			if (!name) {
+				console.log(value);
+			} else console.log({ [name]: value });
+	}, [value]);
+	return value;
+}
+
+export function useConsoleAdmin(value: any, name: string | undefined = undefined) {
+	const [isAdmin] = useRecoilState(isAdminState);
+	useEffect(() => {
+		if (isAdmin)
 			if (!name) {
 				console.log(value);
 			} else console.log({ [name]: value });
