1d318721cdc745b568cfc58d97a680e2be9765a0
Citysquirrel <tok1324@naver.com>
2025-02-16 00:24:38 +0900

    타임라인 에디터 기본 틀

--- src/pages/MultiView/TimelineEditor.tsx
+++ src/pages/MultiView/TimelineEditor.tsx
@@ -6,11 +6,47 @@
 //TODO: 본 에디터는 새 창으로 작동할 것임.
 //TODO: 변경 사항은 로컬 스토리지에 원본 형식으로 저장
 
+import { Box, Input, Stack, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { useState } from "react";
+
 export function TimelineEditor() {
-	return <></>;
+	const [data, setData] = useState<TimelineEditorData[]>([]);
+
+	const handleInputValue =
+		(row: number, col: keyof Omit<TimelineEditorData, "uuid">) => (e: React.ChangeEvent<HTMLInputElement>) => {
+			const { value } = e.target;
+			setData((prev) => prev.map((item, idx) => (idx === row ? { ...item, [col]: value } : item)));
+		};
+
+	return (
+		<Box>
+			<Table>
+				<Thead>
+					<Tr>
+						<Th>Timeline</Th>
+						<Th>Text</Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					{data.map((row, idx) => (
+						<Tr key={row.uuid}>
+							<Td>
+								<Input value={row.timeline} onChange={handleInputValue(idx, "timeline")} />
+								{/* 해당 인풋을 (hh)hh:mm:ss 형태로 변경 */}
+							</Td>
+							<Td>
+								<Input value={row.text} onChange={handleInputValue(idx, "text")} />
+							</Td>
+						</Tr>
+					))}
+				</Tbody>
+			</Table>
+		</Box>
+	);
 }
 
-interface Temp {
+interface TimelineEditorData {
+	uuid: string;
 	timeline: string; // (hh)hh:mm:ss 형식.
 	text: string;
 }
