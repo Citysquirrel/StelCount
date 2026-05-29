b2e702704a984de5cd1a3e588373de2466545c7f
citysquirrel <tok1324@naver.com>
2026-05-29 21:18:51 +0900

    어드민 페이지를 lazy 처리: 최소한의 보안 처리

--- src/_router.tsx
+++ src/_router.tsx
@@ -8,16 +8,14 @@ import Home from "./pages/Home";
 import { Counter } from "./pages/Counter";
 import { ServerErrorPage } from "./pages/ServerErrorPage";
 import { MultiView } from "./pages/MultiView";
-import { Admin, AdminEdit } from "./pages/Admin";
-import { NewAdmin } from "./pages/Admin/Main";
 import { Dashboard } from "./pages/Admin/Dashboard";
 import { Stellar } from "./pages/Admin/Stellar";
 import { Songbook } from "./pages/Admin/Songbook";
 import { Setting } from "./pages/Admin/Setting";
 
-// const Admin = lazy(() => import("./pages/Admin").then((m) => ({ default: m.Admin })));
-// const AdminEdit = lazy(() => import("./pages/Admin").then((m) => ({ default: m.AdminEdit })));
-// const NewAdmin = lazy(() => import("./pages/Admin/Main").then((m) => ({ default: m.NewAdmin })));
+const Admin = lazy(() => import("./pages/Admin").then((m) => ({ default: m.Admin })));
+const AdminEdit = lazy(() => import("./pages/Admin").then((m) => ({ default: m.AdminEdit })));
+const NewAdmin = lazy(() => import("./pages/Admin/Main").then((m) => ({ default: m.NewAdmin })));
 
 const devRoutes: RouteObject[] = import.meta.env.DEV ? [] : [];
 
